79f64f0e37e8bc45a279b415d28101552cf25867
pwentz <patrick.w.wentz@gmail.com>
2016-08-29 12:38:10 -0600

    move function calls into doc.ready, still having bug

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -2,40 +2,12 @@ $(function(){ $(document).foundation(); });
 $(document).ready( () => {
   verifyCount();
   verifyEmpty();
-
-  $('.add-task').on('click', () => {
-    let name = $('#task-name').val();
-    let description = $('#task-description').val();
-    localStorage.tasks = Number(localStorage.tasks) + 1;
-    createTask([name, description], localStorage.tasks)
-    clearInput();
-  });
-
-  $('.delete-all').on('click', () => {
-    localStorage.clear();
-    hideTasks();
-  })
-
-  $('.edit-task').on('click', () => {
-    let taskToEdit = event.target.closest('.task-container');
-    toggleForms(taskToEdit);
-    prefillInputs(taskToEdit);
-  });
-
-  $('.save-edit').on('click', () => {
-    let updatedTask = event.target.closest('.task-container');
-    updateTask(updatedTask);
-  });
-  
-  $('.delete-task').on('click', () => {
-    let chosenTask = event.target.closest('.task-container');
-    destroyTask(chosenTask);
-  })
-
-  $('.completed').on('click', () => {
-    let chosenTask = event.target.closest('.task-container');
-    markAsComplete(chosenTask);
-  })
+  addTask();
+  deleteAll();
+  editTask();
+  saveEdit();
+  deleteTask();
+  completeTask();
 })
 
 function verifyCount() {
@@ -53,4 +25,50 @@ function verifyEmpty() {
 function clearInput() {
   $('#task-name').val('')
   $('#task-description').val('')
+}
+
+function addTask() {
+  $('.add-task').on('click', () => {
+    let name = $('#task-name').val();
+    let description = $('#task-description').val();
+    localStorage.tasks = Number(localStorage.tasks) + 1;
+    createTask([name, description], localStorage.tasks)
+    clearInput();
+  });
+}
+
+function deleteAll() {
+  $('.delete-all').on('click', () => {
+    localStorage.clear();
+    hideTasks();
+  })
+}
+
+function editTask() {
+  $('.edit-task').on('click', () => {
+    let taskToEdit = event.target.closest('.task-container');
+    toggleForms(taskToEdit);
+    prefillInputs(taskToEdit);
+  });
+}
+
+function saveEdit() {
+  $('.save-edit').on('click', () => {
+    let updatedTask = event.target.closest('.task-container');
+    updateTask(updatedTask);
+  });
+}
+
+function deleteTask() {
+  $('.delete-task').on('click', () => {
+    let chosenTask = event.target.closest('.task-container');
+    destroyTask(chosenTask);
+  })
+}
+
+function completeTask() {
+  $('.completed').on('click', () => {
+    let chosenTask = event.target.closest('.task-container');
+    markAsComplete(chosenTask);
+  })
 }
